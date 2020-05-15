4b01957cc5e1627ac2125d4c6c3da25f8f17c15f
Maximiano Massariol Cardoso <massariolmc@gmail.com>
2020-05-14 21:04:20 -0400

    Update challenge-016

--- challenge-016/challenge-016.js
+++ challenge-016/challenge-016.js
@@ -19,16 +19,26 @@
     var slug2 = []
     slug.forEach(function(element, index, array){
         slug2.push(element.charAt(0).toUpperCase() + element.slice(1).toLocaleLowerCase());
-    });
+    });//MODO TRADICIONAL
+    console.log('MODO TRADICIONAL: ' + slug2.join(' '));
+
+    slug = newName.split('-').map(function(name){
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }).join(' ');//MODO RÁPIDO
+    console.log('MODO RÁPIDO: ' + slug);        
     
-    console.log(slug2);
-    console.log(slug2.join(' '));
+    
     
     console.log( '\nMeus amigos:' );
     var arr = ['Miguel', 'Labelle', 'Jessica', 'Maximiano'];
     var count_arr = arr.length;    
     var comp2 = arr.slice(0,(count_arr - 1)) + ' e ' +arr.slice(count_arr-1);    
-    console.log("Array: " + comp2);//MANEIRA FÁCIL
+    console.log("MANEIRA FÁCIL: " + comp2);//MANEIRA FÁCIL
+    comp2 = arr.reduce(function(acumulado, atual, index){
+        var separator = arr.length -1 === index ? ' e ' : ', ';
+        return acumulado + separator + atual;
+    }).concat(' são meus amigos.');
+    console.log("MANEIRA MUITO FÁCIL : " + comp2);//MANEIRA MUITO FÁCIL
     var newArr = '';
     for(i = 0; i < count_arr; i++){
         if ((i+2) == count_arr){
@@ -41,16 +51,18 @@
             newArr += (arr[i] + ', ');      
         }        
     }
-    console.log("Array: " + newArr);//MANEIRA DIFICIL
+    console.log("MANEIRA TRADICIONAL : " + newArr);//MANEIRA DIFICIL
     
     console.log( '\nEra "Roberto", agora é:' );
     name = 'Roberto';
     count = name.length;
-    console.log(name.slice(0,2) + name.slice(2,7).replace('o','a'));
+    console.log('MANEIRA 1: ' + name.slice(0,2) + name.slice(2,7).replace('o','a'));
+    console.log('MANEIRA 2: ' + name.replace('to','ta'));
 
     console.log( '\nParte de uma string:' );
     name = 'Fernando';
-    console.log(name.slice(name.lastIndexOf('nando')));
+    console.log('MANEIRA 1: ' + name.slice(name.lastIndexOf('nando')));
+    console.log('MANEIRA 2: ' + name.substring(8,3));
 
     console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
     var myName = 'gabriEla';
